08ff25c671928769c2a10fd5f62dc8c4da6ec1e8
richardk-code <richard.kruemmel@code.berlin>
2020-05-06 19:09:05 +0200

    Introducing white theme, adding about page, adding database, models and additional routes

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config(object):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or '630dd5c25ceeb8031c1391420e1387bf'
